92a7891448ef0322a501635480e9c7206609c95e
Hitesh <hkowdiki@gmail.com>
2021-09-04 21:24:21 +0530

    :white_check_mark: Quests Done

--- src/components/CreateFeed.js
+++ src/components/CreateFeed.js
@@ -1,6 +1,6 @@
 import React from "react";
-import './CreateFeed.css';
-import Post from './Post';
+import "./CreateFeed.css";
+import Post from "./Post";
 import PostBox from "./Postfeed";
 import FlipMove from "react-flip-move";
 import Feed from "../data/feed";
@@ -8,28 +8,28 @@ import Feed from "../data/feed";
 const CreateFeed = () => {
   return (
     <React.Fragment>
-    <div className="feed">
-      <div className="feed__header">
-        <h2>Home</h2>
+      <div className="feed custom">
+        <div className="feed__header">
+          <h2>Home</h2>
+        </div>
+
+        <PostBox />
+
+        <FlipMove>
+          {Feed.map((data, key) => (
+            <Post
+              key={key}
+              username={data.username}
+              verified="True"
+              text={data.caption}
+              image={data.postImg}
+              avatar={data.profilePicture}
+            />
+          ))}
+        </FlipMove>
       </div>
-
-      <PostBox />
-
-      <FlipMove>
-      {Feed.map((data, key) => 
-          <Post
-            key={key}
-            username={data.username}
-            verified='True'
-            text={data.caption}
-            image={data.postImg}
-            avatar={data.profilePicture}
-          />
-      )}
-      </FlipMove>
-    </div>
     </React.Fragment>
   );
-}
+};
 
 export default CreateFeed;
--- src/components/Quests.js
+++ src/components/Quests.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import questData from "../data/quests";
+import Quest from "./Quest";
 import "./Quests.css";
 
 const Quests = () => {
@@ -13,15 +14,14 @@ const Quests = () => {
       </div>
       <ul>
         {quests.day1.map((quest, key) => (
-          <li className="checked">
-            <input type="checkbox" />
-            <label>
-              <div className="checkbox">
-                <span className="fa fa-check"></span>
-              </div>
-              <span className="item-name">{quest}</span>
-            </label>
-          </li>
+          // <li className="checked">
+          //   {/* <input type="checkbox" /> */}
+          //   <Checkbox checked={checked} onChange={handleChange} />
+          //   <label>
+          //     <span className="item-name">{quest}</span>
+          //   </label>
+          // </li>
+          <Quest data={quest} />
         ))}
       </ul>
     </div>
