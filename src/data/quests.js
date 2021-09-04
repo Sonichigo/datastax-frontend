69983c0b7974b1a487c39ac625690ba287877376
Hitesh Kowdiki <61322830+kkhitesh@users.noreply.github.com>
2021-09-05 00:07:02 +0530

    ⚙ API Integrated

--- src/data/quests.js
+++ src/data/quests.js
@@ -1,6 +1,6 @@
 const quests = {
   id: "015db679-8d8f-46c0-9358-667ebf7211d2",
-  task: ["Take picture of a bird", "Take a pic of nature", "go on a hike"],
+  task: ["Go for a Jog", "Listen to Music", "Paint"],
 };
 
 export default quests;
